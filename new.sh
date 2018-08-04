9e657cd584fd1fd3a09a2787c21873eb46a5e335
Jon Craton <jncraton@gmail.com>
2018-08-04 14:44:25 -0400

    Initialize a fresh git repo on project creation

--- new.sh
+++ new.sh
@@ -1,7 +1,21 @@
 #!/bin/bash
 
+# Checkout the template to the project-template subdir
 git clone --depth 1 https://github.com/jncraton/project-template.git
+
+# Move the files from the template here
 mv project-template/* .
+mv project-template/.travis.yml .
+mv project-template/.gitignore .
+
+# Get rid of the subdir
 rm -rf project-template
+
+# Truncate the readme
 rm -f readme.md
 touch readme.md
+
+# Create a new repo here
+git init
+git add * .gitignore .travis.yml
+git commit -a -m "Initial commit"
